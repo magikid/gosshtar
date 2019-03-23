2483205be700a73281e01095e154e228a73d5035
Chris W Jones <chris.jones@cbsinteractive.com>
2019-03-22 21:56:05 -0400

    Adds logrus for logging

--- gosshtar.go
+++ gosshtar.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
+	log "github.com/sirupsen/logrus"
 	"math/rand"
 	"net"
+	"os"
 	"time"
 )
 
 func handleConnection(conn net.Conn) {
-	log.Printf("Connection opened for %v", conn.RemoteAddr())
+	log.Infof("Connection opened for %v", conn.RemoteAddr())
 	for {
 		output := fmt.Sprintf("%v\r\n", rand.Int())
-		log.Print("Wasting 10 seconds: ", output)
+		log.Debugf("Wasting 10 seconds: %v", output)
 		_, err := conn.Write([]byte(output))
 		if err != nil {
-			log.Printf("Connection closed for %v", conn.RemoteAddr())
+			log.Infof("Connection closed for %v", conn.RemoteAddr())
 			break
 		}
 		time.Sleep(10 * time.Second)
@@ -23,7 +24,17 @@ func handleConnection(conn net.Conn) {
 }
 
 func main() {
-	log.Print("Starting ssh tar pit")
+	Formatter := new(log.TextFormatter)
+	Formatter.TimestampFormat = "02-01-2006 15:04:05"
+	Formatter.FullTimestamp = true
+	log.SetFormatter(Formatter)
+
+	debug := os.Getenv("DEBUG")
+	if debug != "" {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	log.Info("Starting ssh tar pit")
 	ln, err := net.Listen("tcp", ":2222")
 
 	if err != nil {
@@ -33,7 +44,7 @@ func main() {
 	defer ln.Close()
 
 	for {
-		log.Print("Starting to accept new connections")
+		log.Info("Starting to accept new connections")
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Fatal(err)
